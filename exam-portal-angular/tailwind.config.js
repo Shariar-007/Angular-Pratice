4cfc54517343efc10979d181e462e7c0606a3b6c
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2022-12-05 21:11:48 +0600

    frontend-examportal added tailwindcss

---
+++ exam-portal-angular/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // purge: {
+  //   enabled: true,
+  //   content: ['./src/**/*.html', './src/**/*.ts']
+  // },
+  // darkMode: false, // or 'media' or 'class'
+  // content: [],
+  // theme: {
+  //   extend: {},
+  // },
+  // variants: {
+  //   extend: {}
+  // },
+  // plugins: [],
+}
