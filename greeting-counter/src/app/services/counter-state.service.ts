141798b0af06191118138e346dfcd8239b40626c
Shariar Shohag <uiu.151007@gmail.com>
2026-07-13 05:23:12 -0400

    added signal() feature and used it

--- greeting-counter/src/app/services/counter-state.service.ts
+++ greeting-counter/src/app/services/counter-state.service.ts
@@ -1,5 +1,5 @@
 
-import {Injectable} from '@angular/core';
+import {Injectable, signal, computed} from '@angular/core';
 import { LoggerService } from './logger.service';
 
 @Injectable({
@@ -7,21 +7,35 @@ import { LoggerService } from './logger.service';
 })
 
 export class CounterStateService{
-  private count = 0;
+  // private count = 0;
+  private countSignal = signal(0); // the reactive container
+  readonly count = this.countSignal.asReadonly();
+  readonly isEven = computed(() => this.countSignal() % 2 === 0);
 
   constructor(private logger: LoggerService) {}   // ← injecting a service into a service
 
-  getCount(): number {
-    return this.count;
-  }
+  // getCount(): number {
+  //   return this.count;
+  // }
+
+  // increment() {
+  //   this.count++;
+  //   this.logger.log(`Count incremented to ${this.count}`);
+  // }
+
+  // reset() {
+  //   this.count = 0;
+  //   this.logger.log('Count reset');
+  // }
 
   increment() {
-    this.count++;
-    this.logger.log(`Count incremented to ${this.count}`);
+    this.countSignal.update(c => c + 1);
+    this.logger.log(`Count incremented to ${this.countSignal()}`);
   }
 
   reset() {
-    this.count = 0;
+    this.countSignal.set(0);
     this.logger.log('Count reset');
   }
+
 }
