664c1a127552c08449c7dd1a686bb99299da424a
Shariar Shohag <uiu.151007@gmail.com>
2026-07-26 01:15:03 -0400

    pure pipe = true/ false reasoning

--- greeting-counter/src/app/pipes/truncate.pipe.ts
+++ greeting-counter/src/app/pipes/truncate.pipe.ts
@@ -1,14 +1,13 @@
 
-import {Pipe, PipeTransform} from "@angular/core";
+ import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   standalone: false,
   name: 'truncate'
 })
 
-export class TruncatePipe implements PipeTransform {
-  transform(value: string, limit: number = 20): string{
-    if(!value) return '';
+export class TruncatePipe implements PipeTransform{
+  transform(value: string, limit: number = 20){
     return value.length > limit ? value.substring(0, limit) + '...': value;
   }
 }
--- greeting-counter/src/app/pipes/truncateNew.pipe.ts
+++ greeting-counter/src/app/pipes/truncateNew.pipe.ts
@@ -1,12 +1,19 @@
 
 import {Pipe, PipeTransform} from '@angular/core';
+
 @Pipe({
   standalone: false,
-  name: 'truncateNew'
+  name: 'truncateNew',
+  pure: false
 })
 
 export class TruncateNewPipe implements PipeTransform{
   transform(value: string, limit: number = 10){
+    console.log('truncate pipe ran');
     return value.length > limit ? value.substring(0, limit) + '...' : value;
   }
 }
+
+
+
+
